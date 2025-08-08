f02f05915ee7a36550526dee7ff77773812c48cb
Damma65 <michael@westcoasthouse.se>
2025-08-08 21:54:40 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,2 @@
-self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open('latmasken-v1').then(cache=>cache.addAll(['/', '/manifest.webmanifest'])));
-});
-self.addEventListener('fetch', (e)=>{
-  e.respondWith(caches.match(e.request).then(res=> res || fetch(e.request)));
-});
+self.addEventListener('install',(e)=>{e.waitUntil(caches.open('latmasken-v1').then(c=>c.addAll(['/','/manifest.webmanifest'])))});
+self.addEventListener('fetch',(e)=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
